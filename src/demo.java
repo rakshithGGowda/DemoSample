763e24f7e3ad3dba276272e575bcc534713d32d0
rakg <rakg@WKWIN7494078.global.publicisgroupe.net>
2020-03-17 22:51:25 +0530

    first commit

---
+++ src/demo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author rakg
+ *
+ */
+public class demo {
+
+	/**
+	 * 
+	 */
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
